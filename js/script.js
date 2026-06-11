960405b71923a6064121d8f7c52de79cfe877c56
Hejpa <alekh.kaur.2@gmail.com>
2026-06-11 17:08:10 +0200

    -
    -

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 document.addEventListener("keydown", (event)=>{
+    const childDiv = document.getElementById(event.key);
+    const parentDiv = childDiv.parentNode;
 
-    if(event.key=="a"){
-        document.getElementById("a").style.backgroundColor="red"
+    switch (parentDiv.className){
+        case "row1":
+            childDiv.style.backgroundColor="purple";
+        break;
+        case "row2":
+         childDiv.style.backgroundColor="blue";
+        break;
+         case "row3":
+            childDiv.style.backgroundColor="pink";
+        break;
+         case "row4":
+            childDiv.style.backgroundColor="purple";
+        break;
+         case "row5":
+            childDiv.style.backgroundColor="blue";
+        break;
+         case "row6":
+            childDiv.style.backgroundColor="pink";
+        break;
+      
+
     }
+})
 
-
+document.addEventListener("keyup", (event)=>{   
+    document.getElementById(event.key).style.removeProperty("background-color");
 })
